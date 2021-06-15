e78cbb1da83a87f819cf0a5d4eddd00fe8cf5032
Yule Show <yuleshow@mac.com>
2021-06-15 15:49:10 -0700

    updated 20210615

---
+++ elpa/bing-dict-20200216.110/bing-dict-autoloads.el
@@ -0,0 +1,39 @@
+;;; bing-dict-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "bing-dict" "bing-dict.el" (0 0 0 0))
+;;; Generated autoloads from bing-dict.el
+
+(autoload 'bing-dict-brief "bing-dict" "\
+Show the explanation of WORD from Bing in the echo area.
+
+\(fn WORD &optional SYNC-P)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "bing-dict" '("bing-dict-")))
+
+;;;***
+
+;;;### (autoloads nil "bing-dict-cache" "bing-dict-cache.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from bing-dict-cache.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "bing-dict-cache" '("bing-dict-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("bing-dict-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; bing-dict-autoloads.el ends here
